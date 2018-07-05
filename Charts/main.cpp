1b8bb5bd738a7e43d68ba6cf8e41f0874a938594
Apple <gustavocampos@gustavos-mac-mini.local>
2018-07-05 00:32:44 -0300

    image

--- Charts/main.cpp
+++ Charts/main.cpp
@@ -21,6 +21,7 @@
 #include "Util.hpp"
 
 #include "Images/ericsson.xpm"
+#include "Images/ericsson_logo.xpm"
 
 int main(int argc, const char * argv[])
 {
@@ -65,6 +66,7 @@ int main(int argc, const char * argv[])
     
     
     gChartContext.DrawXPM((nWidth/2)-(250/2), (nHeight/2)-(51/2), 1, 1, 250, 51, 240, (const char**) ericsson_landscape3);
+    gChartContext.DrawXPM(nWidth-51, nHeight-51, 1, 1, 50, 50, 10, (const char**) ericsson_logo);
     
     gChartContext.SelectDefaultFont(0);
     gChartContext.GPrint(5, 14, 0, MkColor(0,0,0), strChartName.c_str());
